fe12e4f96a5aadae39e3ba824a3baf8c1ae03646
SuchithraSuchithra <suchitra.nikki@gmail.com>
2021-07-07 21:20:22 +1000

    5 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Input } from '@angular/core';
 import { Product } from '../products';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
-  templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css']
+  templateUrl: 'product-alerts.component.html',
+  styleUrls: ['product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-  @Input() product! : Product; 
+  @Input() product!: Product | undefined;
+  @Output() notify = new EventEmitter();
   constructor() {}
 
   ngOnInit() {}
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { products } from '../products';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-list',
@@ -8,13 +8,16 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
-  products = products;
+  products: Product[] = products;
 
   share() {
     window.alert('The product has been shared!');
   }
-}
 
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
 
 /*
 Copyright Google LLC. All Rights Reserved.
